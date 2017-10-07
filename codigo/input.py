074f6891792d43ec6ebd4f8225f1c161530403e1
Enrique <eiglesias34@gmail.com>
2017-10-07 15:52:10 -0430

    Anadio los problemas, archivo categorizando cada problema por tamano, soluciones optimas y codigo de carga matrices

---
+++ codigo/input.py
@@ -0,0 +1,41 @@
+import sys, traceback, random, time
+from math import *
+
+def parseArgs(args):
+    msg = "Error en la linea de comando:\arbolbinario  <nombre de archivo> <nombre del nuevo archivo>"
+    if len(args) != 2:
+        print(msg)
+        sys.exit(1)
+    return args[1]
+
+def lectura_de_matrices(fent):
+	myfile = open(fent, "r")
+	num_ubicaciones = int(next(myfile))
+	distancia = [[0 for x in range(num_ubicaciones)] for y in range(num_ubicaciones)] 
+	flujo = [[0 for x in range(num_ubicaciones)] for y in range(num_ubicaciones)] 
+	next(myfile)
+
+	x = 0 
+	for line in myfile:
+		line = line.strip('\n')
+		elementos = line.split(' ')
+		for y in xrange(num_ubicaciones):
+			distancia[x][y] = int(elementos[y])
+		x+=1
+		if x == num_ubicaciones:
+			break
+
+	print "ya pase"
+	next(myfile)
+	x = 0 
+	for line in myfile:
+		line = line.strip('\n')
+		elementos = line.split(' ')
+		for y in xrange(num_ubicaciones):
+			flujo[x][y] = int(elementos[y])
+		x+=1
+		if x == num_ubicaciones:
+			break
+
+	return distancia, flujo
+
