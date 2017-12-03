4d5ff8a9c1d5cd62ff0fcc48a9b42aafb309f386
Andrea Centeno <andreacent8@gmail.com>
2017-12-03 18:36:14 -0400

    modificaciones ant colony

--- AntColony.py
+++ AntColony.py
@@ -1,7 +1,6 @@
 from QAP_Poblacion import Solution,QAP_Poblacion as QAP
 from LocalSearch import LocalSearch
 from random import randint,random,sample,uniform
-from random import shuffle
 
 Q = 100
 q = 0.9
@@ -9,18 +8,21 @@ alpha = 0.2
 
 class AntColony(QAP):
 
-	def objectiveFunc(self,sol,solLen,distance,flow):
-		value = 0
-		for i in range(solLen):
-			for j in range(solLen):
-				value += flow[sol[i]-1][sol[j]-1]*distance[i][j]
-		return value
-
 	def updatePheromeTrail(self, PT, sol, solValue):
+		'''
+		actualiza el rastro de feromonas
+
+		Parametros:
+		PT  - matriz de feromonas
+		sol - solucion
+		solValue - valor de funcion objetivo 
+		'''
+		#evapora
 		for i in range(0, self.number):
 			for j in range(0, self.number):
 				PT[i][j] = (1 - alpha) * PT[i][j]
 
+		#refuerza rastro
 		for i in range(0, self.number):
 			PT[i][sol[i]-1] = PT[i][sol[i]-1] + alpha/solValue
 
@@ -38,6 +40,13 @@ class AntColony(QAP):
 		return dividor
 
 	def permutation(self, sol, PT):
+		'''
+		Permuta componentes segun cierta probabilidad
+
+		Parametros:
+		PT  - matriz de feromonas
+		sol - solucion
+		'''
 
 		randSwap = sample(range(len(sol)), int(len(sol)/3))
 		i = randint(0, self.number-1)
@@ -61,15 +70,21 @@ class AntColony(QAP):
 
 
 	def createPheromeTrail(self, solValue):
+		'''
+		Crea la matriz de feromonas inicial
+
+		Parametros:
+		solValue -- mejor valor de la poblacion inicial
+		'''
 
 		PT = [[1/(solValue*Q) for x in range(self.number)] for y in range(self.number)] 
 		return PT
 	
 	def colony(self, numIter):
-
-		noImprovement = 0
 		PT = self.createPheromeTrail(self.bestvalue)
-		Intensification = True
+		noImprovement = 0
+		intensification = True
+
 		for x in range(numIter):
 			improvement = False
 
@@ -77,24 +92,24 @@ class AntColony(QAP):
 				ant = Solution(self.number,self.distance,self.flow, self.population[pos]) 
 
 				ant.sol = self.permutation(ant.sol, PT)
-				ant.solValue = self.objectiveFunc(ant.sol, self.number, self.distance, self.flow)
+				ant.solValue = ant.objectiveFunc(ant.sol, self.number, self.distance, self.flow)
 
 				qap = LocalSearch(self.number, self.distance, self.flow, self.optValue, ant.sol)
 				qap.localSearchFirst(numIter)
 				ant.sol, ant.solValue = qap.sol[:], qap.solValue 
-				if Intensification:
-					if ant.solValue < self.population[pos].solValue:
-						self.population[pos].sol, self.population[pos].solValue = ant.sol, ant.solValue	
-					else:
-						continue
-				else:
-					self.population[pos].sol, self.population[pos].solValue = ant.sol, ant.solValue
 
-			for pos in range(len(self.population)):
-				if self.population[pos].solValue < self.bestvalue:
-					self.bestindividual, self.bestvalue = self.getBestIndividual()
+				#no intensifica o (si intensifica y la solucion es mejor que la anterior)
+				if not intensification or ant.solValue < self.population[pos].solValue:
+					self.population[pos].sol, self.population[pos].solValue = ant.sol, ant.solValue	
+				else:
+					continue 
+
+			#for ant in self.population:
+				#mejora la solucion global
+				if ant.solValue < self.bestvalue:
+					self.bestindividual, self.bestvalue = ant.sol, ant.solValue
 					improvement = True
-					Intensification = True
+					intensification = True
 
 			PT = self.updatePheromeTrail(PT, self.bestindividual, self.bestvalue)
 
